85d5e64f07d129088dd31dc3864d059a17ff7aec
Maxime <Maxime@Yirumax-PC>
2014-11-14 16:41:30 +0100

    Commit de l'interface

--- src/users/Utilisateur.java
+++ src/users/Utilisateur.java
@@ -5,6 +5,15 @@
  */
 package users;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Maxime
@@ -31,10 +40,51 @@ public class Utilisateur {
         this.motDePasse = motDePasse;
     }
     
-    private static boolean nomEtMdpCorrect(String id, String mdp) {
+    public static boolean inscription(String id, String mdp) {
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(new FileWriter("mdp.txt", true));
+            pw.println(id + " " + mdp);
+            pw.close();
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(Utilisateur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public static boolean nomEtMdpCorrect(String id, String mdp) {
         boolean res = false;
         
-        return res;
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader("mdp.txt"));
+            
+            while(br.ready()) {
+                String line = br.readLine();
+                String[] lines = line.split(" ");
+                
+                if(lines.length == 1) {
+                    /* Pas d'espace */
+                    if(mdp.equals("") && id.equals(line)) {
+                        return true;
+                    }
+                } else if(lines.length == 2) {
+                    if(mdp.equals(lines[1]) && id.equals(lines[0])) {
+                        return true;
+                    }
+                }
+            }
+            
+            br.close();
+        } catch (FileNotFoundException ex) {
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Utilisateur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
     }
     
     
